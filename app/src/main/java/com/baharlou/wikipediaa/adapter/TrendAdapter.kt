5189343bd4b42f29bc02ff3b8b718bc746dd29b9
mona-baharlou <mona.baharlou755@gmail.com>
2024-05-19 00:38:47 +0330

    TrendAdapter.kt added & TrendFragment updated

---
+++ app/src/main/java/com/baharlou/wikipediaa/adapter/TrendAdapter.kt
@@ -0,0 +1,50 @@
+package com.baharlou.wikipediaa.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.baharlou.wikipediaa.data.ItemPost
+import com.baharlou.wikipediaa.databinding.ItemTrendBinding
+import com.bumptech.glide.Glide
+
+class TrendAdapter(private val data: ArrayList<ItemPost>) :
+    RecyclerView.Adapter<TrendAdapter.TrendViewHolder>() {
+
+    lateinit var binding: ItemTrendBinding
+
+    inner class TrendViewHolder(iteView: View) : RecyclerView.ViewHolder(iteView) {
+        fun bindViews(itemPost: ItemPost) {
+
+            Glide.with(itemView.context)
+                .load(itemPost.imgUrl)
+                .into(binding.itemTrendImg)
+
+            binding.itemTrendSubject.text = itemPost.title
+            binding.itemTrendCount.text = (adapterPosition + 1).toString()
+            binding.itemTrendSubtitle.text = itemPost.subtitle
+            binding.itemTrendInsight.text = itemPost.insight
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrendViewHolder {
+        binding = ItemTrendBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent, false
+
+        )
+
+        return TrendViewHolder(binding.root)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onBindViewHolder(holder: TrendViewHolder, position: Int) {
+        holder.bindViews(data[position])
+    }
+
+
+}
--- app/src/main/java/com/baharlou/wikipediaa/fragments/FragmentTrend.kt
+++ app/src/main/java/com/baharlou/wikipediaa/fragments/FragmentTrend.kt
@@ -5,11 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.baharlou.wikipediaa.adapter.ExploreAdapter
+import com.baharlou.wikipediaa.adapter.TrendAdapter
 import com.baharlou.wikipediaa.databinding.FragmentProfileBinding
 import com.baharlou.wikipediaa.databinding.FragmentTrendBinding
 
 class FragmentTrend : Fragment() {
     lateinit var binding: FragmentTrendBinding
+    lateinit var adapter: TrendAdapter
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -21,5 +26,11 @@ class FragmentTrend : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        adapter = TrendAdapter(Util.trendItems)
+        binding.recyclerTrend.layoutManager =
+            LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        binding.recyclerTrend.adapter = adapter
+
     }
 }
