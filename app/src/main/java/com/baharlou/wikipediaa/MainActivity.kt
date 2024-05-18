ace5d61d110e2a2c5f95a87f19e8f7f9a933fbc3
mona-baharlou <mona.baharlou755@gmail.com>
2024-05-18 14:42:05 +0330

    MainActivity.kt updated

--- app/src/main/java/com/baharlou/wikipediaa/MainActivity.kt
+++ app/src/main/java/com/baharlou/wikipediaa/MainActivity.kt
@@ -3,6 +3,7 @@ package com.baharlou.wikipediaa
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.baharlou.wikipediaa.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +16,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setToolbar()
+
+        toggleDrawer()
+    }
+
+    private fun toggleDrawer() {
+       /* val actionBarDrawerToggle: ActionBarDrawerToggle(
+        this,
+        binding.drawerLayoutMain,
+        binding.toolbarMain,
+        R.string.open_drawer,
+        R.string.close_drawer,
+
+        )*/
     }
 
     private fun setToolbar() {
