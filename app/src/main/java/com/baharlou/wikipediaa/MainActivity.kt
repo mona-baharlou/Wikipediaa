ae32241942fc0951cd9a91896affb8481f230ed5
mona-baharlou <mona.baharlou755@gmail.com>
2024-05-18 14:49:58 +0330

    MainActivity.kt updated

--- app/src/main/java/com/baharlou/wikipediaa/MainActivity.kt
+++ app/src/main/java/com/baharlou/wikipediaa/MainActivity.kt
@@ -18,17 +18,59 @@ class MainActivity : AppCompatActivity() {
         setToolbar()
 
         toggleDrawer()
+
+        setMenu()
+    }
+
+    private fun setMenu() {
+        binding.navigationViewMain.setNavigationItemSelectedListener {
+
+            when (it.itemId) {
+
+                R.id.menu_writer -> {
+
+                }
+
+                R.id.menu_photographer -> {
+
+                }
+
+                R.id.menu_video_maker -> {
+
+                }
+
+                R.id.menu_translator -> {
+
+                }
+
+                ///----------------------------------------------/
+
+                R.id.menu_open_wikimedia -> {
+
+                }
+
+                R.id.menu_open_wikipedia -> {
+
+                }
+            }
+
+            true
+        }
+
     }
 
     private fun toggleDrawer() {
-       /* val actionBarDrawerToggle: ActionBarDrawerToggle(
-        this,
-        binding.drawerLayoutMain,
-        binding.toolbarMain,
-        R.string.open_drawer,
-        R.string.close_drawer,
+        val actionBarDrawerToggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayoutMain,
+            binding.toolbarMain,
+            R.string.open_drawer,
+            R.string.close_drawer
+        )
 
-        )*/
+        binding.drawerLayoutMain.addDrawerListener(actionBarDrawerToggle)
+
+
     }
 
     private fun setToolbar() {
