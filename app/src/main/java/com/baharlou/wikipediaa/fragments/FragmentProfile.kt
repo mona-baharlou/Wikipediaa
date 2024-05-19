e07ad80398f1195a0e97a140456a0b88ac7181f2
mona-baharlou <mona.baharlou755@gmail.com>
2024-05-19 13:56:24 +0330

    profile updated

--- app/src/main/java/com/baharlou/wikipediaa/fragments/FragmentProfile.kt
+++ app/src/main/java/com/baharlou/wikipediaa/fragments/FragmentProfile.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.baharlou.wikipediaa.databinding.FragmentProfileBinding
 
@@ -20,5 +22,21 @@ class FragmentProfile : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setToolbar()
+        setCollapsing()
+    }
+
+
+    private fun setCollapsing() {
+        binding.collapsingMain.setExpandedTitleColor(
+            ContextCompat.getColor(requireContext(), android.R.color.transparent)
+        )
+        binding.collapsingMain.title = "About Me"
+    }
+
+    private fun setToolbar() {
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbarMain)
+
     }
 }
